578f295b5e8b0d7d124eea8fbe15f01c9c2bff75
Md. Mansurul Haque <mansurul.haque1997@gmail.com>
2025-10-27 21:32:14 +0600

    next page redirect

--- script/login.js
+++ script/login.js
@@ -1,1 +1,16 @@
-alert()
+document.getElementById("login-btn").addEventListener("click",function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("Account-number").value;
+    const Pin = document.getElementById("pin").value;
+    if (accountNumber.length === 11) {
+        if (Pin==="1111"){
+            window.location.href = "./main.html";
+        }
+        else {
+            console.log("Invalid Pin");
+        }
+    }
+    else {
+        console.log("Need valid account number");
+    }
+})
