df97513fcc9b01997d87ff6a34e67e0089301503
Md. Mansurul Haque <mansurul.haque1997@gmail.com>
2025-10-30 21:13:04 +0600

    a simple problem solved

--- script/addmoney.js
+++ script/addmoney.js
@@ -1,11 +1,15 @@
-alert("Successfully Logged In");
+// alert("Successfully Logged In");
 
 document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
-    const amount = document.getElementById("amount").value;
-    const piin = document.getElementById("pin").value;
-    if(piin===1111) {
-        console.log("Okay add kora jabe")
+    const amount = document.getElementById("Amount").value;
+    console.log(amount);
+    const Pin = document.getElementById("pin").value;
+    console.log(Pin);
+    const convertedPin = parseInt(pin);
+    console.log(convertedPin);
+    if(convertedPin==1111) {
+        console.log("Okay add kora jabe");
     }
     else {
         console.log("Invalid pin number");
