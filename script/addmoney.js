bd2ae8fdfe1c8447b3309a7c2ea5988241c36cc8
Md. Mansurul Haque <mansurul.haque1997@gmail.com>
2025-11-03 20:40:29 +0600

    add money

--- script/addmoney.js
+++ script/addmoney.js
@@ -3,15 +3,22 @@
 document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
     const amount = document.getElementById("Amount").value;
-    console.log(amount);
+    const convertedamount = parseFloat(amount);
+    const Money = document.getElementById("money").innerText;
+    const convertedmoney = parseFloat(Money);
     const Pin = document.getElementById("pin").value;
-    console.log(Pin);
-    const convertedPin = parseInt(pin);
-    console.log(convertedPin);
-    if(convertedPin==1111) {
-        console.log("Okay add kora jabe");
+    const convertedPin = parseInt(Pin);
+    if(amount && Pin) {
+        if(convertedPin==1111){
+            total = convertedamount + convertedmoney;
+            document.getElementById("money").innerText=total;
+            console.log(total);
+        }
+        else{
+            alert("Enter valid pin");
+        }
     }
     else {
-        console.log("Invalid pin number");
+        alert("Enter amount");
     }
 })
